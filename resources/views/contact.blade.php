c993726af8a9de9b30501e5cfb14b03930b89b4b
Michael Roth <michael.franklin.roth@gmail.com>
2019-09-19 18:20:12 -0400

    my first laravel site

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+    <h1>Contact Form</h1>
+
+    <p>
+      <a href="">Contact us</a> to learn more!
+    </p>
+</body>
+</html>
