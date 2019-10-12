745204d261d4053e7a31c1bec9d548337b93ad2d
Michael Roth <michael.franklin.roth@gmail.com>
2019-10-12 11:26:23 -0400

    more practice with forms in laravel

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Project;
 use Illuminate\Http\Request;
 
 class ProjectsController extends Controller
@@ -11,5 +12,22 @@ class ProjectsController extends Controller
         $projects = \App\Project::all();
 
         return view('projects.index', ['projects' => $projects]);
+    }
+
+    public function create()
+    {
+        return view('projects.create');
+    }
+
+    public function store()
+    {
+        $project = new Project();
+
+        $project->title = request('title');
+        $project->description = request('description');
+
+        $project->save();
+
+        return redirect('/projects');
     }
 }
